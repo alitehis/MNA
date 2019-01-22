6a109d5b2309026c9deb92e44caa0ff6fda27e25
Ali khoshtinattt <45663442+alitehis@users.noreply.github.com>
2019-01-22 16:05:30 +0330

    First Commit

---
+++ Component.py
@@ -0,0 +1,28 @@
+from enum import Enum
+import Node
+
+
+class TypeOfComp(Enum):
+    Res = "Resistance"
+    Node = "Node"
+    OpAmp = "OpAmp"
+    IndVolt = "Independent voltage"
+    IndCur = "Independent Current"
+
+class Component():
+    def __init__(self, type: TypeOfComp):
+        self.__voltage: int = 0
+        self.__charge: int = 0
+        self.__type = type
+
+    @property
+    def Type(self):
+        return self.__type
+
+def checkExistanceOfNode(nodeNumber, nodes: dict):
+    if nodeNumber in nodes:
+        node = nodes[nodeNumber]
+    else:
+        node = Node.Node(nodeNumber)
+        nodes[nodeNumber] = node
+    return node
